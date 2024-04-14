c789666154304a20a9bac195d0ad9906482910d1
Poorya MohammadiNasab <poorya.m.n.b@gmail.com>
2024-04-14 09:35:37 +0330

    Dice score added

---
+++ utils/metrics.py
@@ -0,0 +1,27 @@
+import torch
+
+def dice_score(predictions, targets):
+    """
+    Calculate the Dice score between predicted and ground truth masks
+
+    Args:
+        predictions: predicted masks
+        targets: Ground truth masks
+
+    Returns:
+        Average Dice score across all elements in the batch
+    """
+    smooth = 1.0
+
+    # flatten predictions and targets
+    predictions = predictions.flatten()
+    targets = targets.flatten()
+
+    # Calculate Intersection and Union
+    intersection = (predictions * targets).sum()
+    union = predictions.sum() + targets.sum()
+
+    # Dice score formula
+    dice = (2 * intersection + smooth) / (union + smooth)
+    
+    return dice
