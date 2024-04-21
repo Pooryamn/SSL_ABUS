1d5aab4984d124901c779cfc778189974f0d9d3f
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-04-21 08:28:59 +0000

    fix a minor bug

--- TRAIN.py
+++ TRAIN.py
@@ -129,8 +129,8 @@ def TRAIN_Func(epochs, batch_size, train_volume_dir, train_mask_dir, test_volume
 if __name__ == 'main':
 
     TRAIN_Func(
-        epochs = sys.argv[1],
-        batch_size = sys.argv[2],
+        epochs = int(sys.argv[1]),
+        batch_size = int(sys.argv[2]),
         train_volume_dir = sys.argv[3],
         train_mask_dir = sys.argv[4],
         test_volume_dir = sys.argv[5],
