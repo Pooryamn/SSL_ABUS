f6a7a238b1cfe6ad6a6719dd2201bcfeef28d470
Poorya MohammadiNasab <poorya.m.n.b@gmail.com>
2024-04-30 14:42:59 +0330

    BCE_Dice v1 added

--- utils/losses.py
+++ utils/losses.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class DiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
@@ -15,3 +16,21 @@ class DiceLoss(nn.Module):
         dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
         
         return 1 - dice
+    
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):     
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth) 
+
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+
+        Dice_BCE = BCE + dice_loss
+        return Dice_BCE
