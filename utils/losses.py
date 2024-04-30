4ad98c63289b09e8b504b267a49344465d4ee09e
Poorya MohammadiNasab <poorya.m.n.b@gmail.com>
2024-04-30 14:44:08 +0330

    IoU loss v1 added

--- utils/losses.py
+++ utils/losses.py
@@ -34,3 +34,23 @@ class DiceBCELoss(nn.Module):
 
         Dice_BCE = BCE + dice_loss
         return Dice_BCE
+    
+class IoULoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return 1 - IoU
