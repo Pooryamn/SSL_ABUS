0111a48ec49659b7aa0c66199fa99bacd43e3835
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-05-06 11:47:40 +0000

    Train Noise file added

---
+++ TRAIN_Noise.py
@@ -0,0 +1,141 @@
+import sys
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+from model.UNET import UNet
+from model.ATT_UNET import Attention_Unet
+
+from utils.dataloader_noise import Data_generator
+from utils.metrics import PSNR
+from skimage.metrics import structural_similarity as ssim
+
+def TRAIN_Func(epochs, batch_size, model, train_volume_dir, validation_volume_dir, feature_maps):
+    
+    # Check GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    # Parameters
+    learning_rate = 0.001
+
+    train_data      = np.load(train_volume_dir)
+    validation_data = np.load(validation_volume_dir)
+
+    if model == "Unet":
+        # Create Model 
+        model = UNet(in_ch=1, out_ch=1, features=feature_maps).to(device)
+
+    elif model == "Attention_Unet":
+        model  = Attention_Unet(in_ch=1, out_ch=1, features=feature_maps).to(device)
+
+    else:
+        raise('Error in selecting the model')
+
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Loss Funcrion
+    criterion = nn.MSELoss()
+    
+    # Trainin Loop
+    for epoch in range(epochs):
+    
+        # Train model
+        model.train()
+    
+        train_loss = 0
+
+        (volumes, masks) = Data_generator(train_data)
+
+        volumes = volumes.to(device)
+        masks = masks.to(device)
+        # Forward Pass
+        outputs = model(volumes)
+        
+        # Memory related function
+        del volumes
+        
+        # Calculate Loss
+        loss = criterion(outputs, masks)
+        train_loss += loss.item()
+
+        # Backward Pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # metrics
+        # convert to numpy first
+        masks   = np.array(masks.squeeze(0).squeeze(0))
+        outputs = np.array(outputs.squeeze(0).squeeze(0))
+
+        Train_ssim_score = ssim(masks, outputs, full=True, data_range=1.0)
+        Train_ssim_score = Train_ssim_score[0]
+
+        Train_PSNR = PSNR(masks, outputs)
+
+
+        print(f"Epoch: {epoch}/{epochs}, batch: {i}/{len(train_dataloader)}, Loss: {loss.item():.4f}, Dice: {dice.item():.5f}")  
+        
+        # Memory related function
+        del masks
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    
+        # Evaluation 
+        model.eval()
+    
+        with torch.no_grad():
+        
+            val_loss = 0
+
+            (volumes, masks) = Data_generator(validation_data)
+        
+            volumes = volumes.to(device)
+            masks   = masks.to(device)
+            
+            # Forward pass
+            outputs = model(volumes)
+            
+            # Memory related function
+            del volumes
+            
+            # Calculate Loss
+            loss = criterion(outputs, masks)
+            val_loss += loss.item()
+            
+            # metrics
+            # convert to numpy first
+            masks   = np.array(masks.squeeze(0).squeeze(0))
+            outputs = np.array(outputs.squeeze(0).squeeze(0))
+
+            Valid_ssim_score = ssim(masks, outputs, full=True, data_range=1.0)
+            Valid_ssim_score = Valid_ssim_score[0]
+
+            Valid_PSNR = PSNR(masks, outputs)
+            
+            # Memory related function
+            del masks
+            gc.collect()
+            torch.cuda.empty_cache()
+        
+        # EPOCH LOG
+        print(f"********** Epoch: {epoch+1}/{epochs},Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}, Train PSNR: {Train_PSNR:.4f}, Validation PSNR: {Valid_PSNR:.4f}, Train SSIM: {Train_ssim_score:.4f}, Validation SSIM: {Valid_ssim_score:.4f}")
+
+    torch.save(model.state_dict(), 'model.pth')
+
+
+
+TRAIN_Func(
+    epochs = 1,
+    batch_size = 1,
+    model = 'Attention_Unet',
+    train_volume_dir = '/teamspace/studios/this_studio/TrainSet/TDSC_Patches/Volumes',
+    train_mask_dir = '/teamspace/studios/this_studio/TrainSet/TDSC_Patches/Mask',
+    test_volume_dir = '/teamspace/studios/this_studio/TestSet/TDSC_Patches/Volumes',
+    test_mask_dir = '/teamspace/studios/this_studio/TestSet/TDSC_Patches/Mask',
+    feature_maps = [16,32,64,128,256]
+    )
