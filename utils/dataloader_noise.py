82e88494860acff8c042ba56409d1d99bd37590f
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-05-07 14:10:40 +0000

    speckle noise formula updated

--- utils/dataloader_noise.py
+++ utils/dataloader_noise.py
@@ -2,19 +2,19 @@ import torch
 import numpy as np
 
 # Function to generate noisy data
-def generate_noisy_data(data, noise_prop):
+def generate_noisy_data(data, noise_std):
     # generate noise
-    noise = np.random.normal(0, 1, data.shape)
+    r = 0.5 
+    noise = np.random.normal(0, noise_std, data.shape)
 
-    noisy_data = data + (noise_prop * noise)
-
+    noisy_data = data + (data ** r) * noise
     return noisy_data
 
 # function to return input (noisy data) and output(original data)
 def Data_generator(data):
 
-    noise_prop = np.random.randint(5,50) / 100
-    noisy_data = generate_noisy_data(data, noise_prop)
+    noise_std = np.random.randint(10,60) / 100
+    noisy_data = generate_noisy_data(data, noise_std)
 
     volume = torch.from_numpy(noisy_data).float().unsqueeze(0).unsqueeze(0)
     mask = torch.from_numpy(data).float().unsqueeze(0).unsqueeze(0)
