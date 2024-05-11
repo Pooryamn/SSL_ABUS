dc93cf14a38b9d619b6aba1a6c7168702aec9d7b
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-05-11 13:39:41 +0000

    Recurrent_block added to R2Unet

--- model/R2UNET.py
+++ model/R2UNET.py
@@ -1,6 +1,32 @@
 import torch
 import torch.nn as nn
 
+class Recurrent_block(nn.Module):
+    """
+    Recurrent block for R2U_Net
+    """
+
+    def __init__(self, out_ch, t=2):
+        super(Recurrent_block, self).__init__()
+
+        self.t = t
+        self.out_ch = out_ch
+
+        self.conv = nn.Sequential(
+            nn.Conv3d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm3d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        for i in range(self.t):
+            if (i == 0):
+                x1 = self.conv(x)
+
+            x1 = self.conv(x + x1)
+        
+        return x1
+
 class R2U_Net(nn.Module):
     """
     R2U-Net model
