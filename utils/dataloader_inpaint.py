9f0897e2583af3c53815c7287a4bbe31daaeea6d
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-05-15 14:16:18 +0000

    some changes in variable names

--- utils/dataloader_inpaint.py
+++ utils/dataloader_inpaint.py
@@ -6,7 +6,10 @@ import random
 
 # A custom class for loading volumes and masks
 class VolumeMaskDataset(torch.utils.data.Dataset):
-    def __init__(self, volume_dir, data_type, n_valid=14):
+    def __init__(self, volume_dir, data_type, n_valid=14, xy_range=[50, 80], z_range=[7, 10]):
+
+        self.xy_range = xy_range
+        self.z_range = z_range
 
         self.file_names = [os.path.join(volume_dir, f) for f in os.listdir(volume_dir)]
         All_idx = np.arange(0, len(self.file_names))
@@ -21,27 +24,27 @@ class VolumeMaskDataset(torch.utils.data.Dataset):
             Train_idx = np.array([x for x in All_idx if x not in Valid_idx])
             self.file_names = [self.file_names[i] for i  in Train_idx]
 
-        self.volume_path = []
+        self.mask_paths = []
         for name in self.file_names:
             for i in range(25):
                 start = str(i * 32)
 
                 for j in range(2):
                     TMP = name + '#' + start + ',' + str(j)
-                    self.volume_path.append(TMP)
+                    self.mask_paths.append(TMP)
         
         # Log
-        print(f'Number of patches for {data_type} set : {len(self.volume_path)}')
+        print(f'Number of patches for {data_type} set : {len(self.mask_paths)}')
 
     def __len__(self):
-        return len(self.volume_paths)
+        return len(self.mask_paths)
     
     def __getitem__(self, idx):
-        volume_path = self.volume_paths[idx]
-        [volume_path, metadata] = volume_path.split('#')
+        mask_path = self.mask_paths[idx]
+        [mask_path, metadata] = mask_path.split('#')
         
         # Load Volume
-        volume = np.load(volume_path)
+        mask = np.load(mask_path)
 
         start, patch_no = metadata.split(',')
         start = int(start)
@@ -49,11 +52,12 @@ class VolumeMaskDataset(torch.utils.data.Dataset):
 
         if patch_no == 0:
             # 0:250
-            volume = volume[:250, :, start:start+32]
+            mask = mask[:250, :, start:start+32]
         else:
             #250:500
-            volume = volume[250:, :, start:start+32]
-            
+            mask = mask[250:, :, start:start+32]
+
+
 
         mask   = np.load(mask_path)
         
@@ -62,6 +66,9 @@ class VolumeMaskDataset(torch.utils.data.Dataset):
         mask   = torch.from_numpy(mask).float().unsqueeze(0)
         
         return volume, mask
+
+    def masking_coordinate_generator(self, img_shape,xy_range=[50, 80], z_range=[7, 10]):
+        pass
 
 def DataLoaderCreator(volume_dir, batch_size, data_type, n_valid=14, shuffle = True):
     # Create dataset
