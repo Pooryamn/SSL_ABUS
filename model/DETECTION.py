9d4afbadff467ce5dff7288f190d6d13ce0f6c2a
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-06-11 17:07:16 +0000

    remove binarization from model's output

--- model/DETECTION.py
+++ model/DETECTION.py
@@ -89,6 +89,6 @@ class Detection_model(nn.Module):
         x = torch.reshape(x, (x.shape[0], 32, 5))
 
         # p is binary
-        x[:, :, 0] = (x[:, :, 0] > self.Threshold) * 1.0
+        #x[:, :, 0] = (x[:, :, 0] > self.Threshold) * 1.0
 
         return x
