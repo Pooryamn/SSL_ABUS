e09f7bbea33b8291f7d20bba2c91fb77e3802cd8
Poorya Mohammdadinasab <poorya.m.n.b@gmail.com>
2024-06-14 19:30:58 +0000

    Postprocessing updated

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -9,9 +9,13 @@ def post_process(x, device):
     kernel = torch.ones([1, 1, 3], dtype=torch.float32).to(device)
 
     for i in range(x.shape[1]):
-        erosion = (F.conv1d(x[:,i,:], kernel, padding=1) == 3) * 1.0
+        x[:,i,:,0] = (x[:,i,:,0] > 0.5) * 1.0
+        erosion = (F.conv1d(x[:,i,:,0], kernel, padding=1) == 3) * 1.0
         opening = (F.conv1d(erosion, kernel, padding=1) > 0) * 1.0
         dilation = (F.conv1d(opening, kernel, padding=1) > 0) * 1.0
-        output[i,:] = (F.conv1d(dilation, kernel, padding=1) == 3) * 1.0
 
+        TMP = ((F.conv1d(dilation, kernel, padding=1) == 3) * 1.0)
+
+        output[i,:,0] = TMP
+        output[i,:,1:] = x[:,i,:,1:]
     return output
